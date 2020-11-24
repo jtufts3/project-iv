4b2863d068b91edb4471276fbe37d700a4de2c55
Jessica Tufts <you@example.com>
2020-11-24 01:59:43 -0500

    Updated and styled pictures, added CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
 
-/* Navbar */
-
-// get the hamburger element
-var hamburger = document.getElementById("hamburger");
-
-// get the menu element
-var menu = document.getElementById("menu");
-
-// get the overlay element
-var overlay = document.getElementById("overlay");
-
-// define toggle function
-function toggleMenu() {
-    // either adds or removes active class from element
-    if (window.innerWidth > 768) return false;
-    menu.classList.toggle("active");
-    overlay.classList.toggle("active");
-}
-
-// execute toggle function from hamburger on click
-hamburger.addEventListener("click", toggleMenu);
-
-// execute toggle function from menu on click
-menu.addEventListener("click", toggleMenu);
-
-// exuecute toggle function from overlay on click
-overlay.addEventListener("click", toggleMenu);
-
-
 /* Modal */
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -38,7 +9,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -55,5 +26,24 @@ window.onclick = function(event) {
   }
 }
 
+/* Graph Modal */
+// Get the modal
+var modal = document.getElementById("graphModal");
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("graph");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("graph-caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("graph-close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
